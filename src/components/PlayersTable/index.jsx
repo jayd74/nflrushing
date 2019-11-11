fff840c2e8cc37675da7ba9b84419cfa83c1b56c
Jason Du <jayd74@gmail.com>
2019-11-11 15:15:20 -0500

    Create PlayersTable with rushing data and pagination

--- src/components/PlayersTable/index.jsx
+++ src/components/PlayersTable/index.jsx
@@ -16,50 +16,6 @@ const PlayersTable = () => {
   const [page, setPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(10);
 
-  let sortedStats
-  const sortData = (category) => {
-    switch (category) {
-      case 'Team':
-        setStats(sortBy(playerData, 'Team'))
-        break;
-      case 'Pos':
-        setStats(sortBy(playerData, 'Pos'))
-        break;
-      case 'Att':
-        setStats(sortBy(playerData, 'Att'))
-        break;
-      case 'Att/G':
-        setStats(sortBy(playerData, 'Att/G'))
-        break;
-      case 'Yds':
-        setStats(sortBy(playerData, 'Yds'))
-        break;
-      case 'TD':
-        setStats(sortBy(playerData, 'TD'))
-        break;
-      case 'Lng':
-        setStats(sortBy(playerData, 'Lng'))
-        break;
-      case '1st':
-        setStats(sortBy(playerData, '1st'))
-        break;
-      case '1st%':
-        setStats(sortBy(playerData, '1st%'))
-        break;
-      case '20+':
-        setStats(sortBy(playerData, '20+'))
-        break;
-      case '40+':
-        setStats(sortBy(playerData, '40+'))
-        break;
-      case 'FUM':
-        setStats(sortBy(playerData, 'FUM'))
-        break;
-      default:
-        setStats(sortedStats)
-    }
-  }
-
   useEffect(() => {
     setStats(playerData)
   }, [])
@@ -67,7 +23,7 @@ const PlayersTable = () => {
 
   const emptyRows = rowsPerPage - Math.min(rowsPerPage, playerData.length - page * rowsPerPage);
 
-  const handleChangePage = (newPage) => {
+  const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
 
@@ -81,18 +37,18 @@ const PlayersTable = () => {
       <TableHead>
         <TableRow>
           <TableCell>Player</TableCell>
-          <TableCell align="right" onClick={() => sortData('Team')}>Team</TableCell>
-          <TableCell align="right" onClick={() => sortData('Pos')}>Pos</TableCell>
-          <TableCell align="right" onClick={() => sortData('Att')}>Att</TableCell>
-          <TableCell align="right" onClick={() => sortData('Att/G')}>Att/G</TableCell>
-          <TableCell align="right" onClick={() => sortData('Yds')}>Yds</TableCell>
-          <TableCell align="right" onClick={() => sortData('TD')}>TD</TableCell>
-          <TableCell align="right" onClick={() => sortData('Lng')}>Lng</TableCell>
-          <TableCell align="right" onClick={() => sortData('1st')}>1st</TableCell>
-          <TableCell align="right" onClick={() => sortData('1st%')}>1st%</TableCell>
-          <TableCell align="right" onClick={() => sortData('20+')}>20+</TableCell>
-          <TableCell align="right" onClick={() => sortData('40+')}>40+</TableCell>
-          <TableCell align="right" onClick={() => sortData('FUM')}>FUM</TableCell>
+          <TableCell align="right">Team</TableCell>
+          <TableCell align="right">Pos</TableCell>
+          <TableCell align="right">Att</TableCell>
+          <TableCell align="right">Att/G</TableCell>
+          <TableCell align="right">Yds</TableCell>
+          <TableCell align="right">TD</TableCell>
+          <TableCell align="right">Lng</TableCell>
+          <TableCell align="right">1st</TableCell>
+          <TableCell align="right">1st%</TableCell>
+          <TableCell align="right">20+</TableCell>
+          <TableCell align="right">40+</TableCell>
+          <TableCell align="right">FUM</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
