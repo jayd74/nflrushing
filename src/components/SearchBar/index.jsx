6ca582514bb10e8b80e30af24492c8e998f10a36
Jason Du <jayd74@gmail.com>
2019-11-13 16:11:11 -0500

    Create SearchBar component

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
+
+const SearchBar = ({setStats, data}) => {
+  const handleSearch = event => {
+    const searchParams = event.target.value.toLowerCase()
+
+    const results = data.filter(result => {
+      const playerName = result["Player"].toLowerCase()
+      return playerName.includes(searchParams)
+    })
+
+    setStats(results)
+  }
+
+  return (
+    <CssTextField
+      id="standard-search"
+      label="Filter players"
+      variant="outlined"
+      type="search"
+      margin="normal"
+      onChange={handleSearch}
+    />
+  )
+}
+
+export default SearchBar
+
+const CssTextField = withStyles({
+  root: {
+    '& .MuiInputLabel-root': {
+      color: 'white'
+    },
+    '& label.Mui-focused': {
+      color: '#0078fd',
+    },
+    '& .MuiOutlinedInput-root': {
+      color: 'white',
+      width: '275px',
+      marginRight: '10px',
+
+      '& fieldset': {
+        borderColor: '#0078fd',
+      },
+      '&:hover fieldset': {
+        borderColor: '#0078fd',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#0078fd',
+      },
+    },
+  },
+})(TextField);
